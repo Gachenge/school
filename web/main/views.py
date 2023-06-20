c24261242191fc9fd92a3a775f504839bab04a13
Gachenge <gachenge1@gmail.com>
2023-06-20 21:11:02 +0300

    add homepage

--- web/main/views.py
+++ web/main/views.py
@@ -3,9 +3,6 @@ from flask import Blueprint, render_template
 main = Blueprint('main', __name__)
 
 @main.route("/")
-def index():
-    return render_template("index.html")
-
 @main.route("/home")
 def home():
     return render_template("home.html")
